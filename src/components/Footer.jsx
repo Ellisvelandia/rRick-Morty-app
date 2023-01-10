25436ef783bed8898f5bac842c5225e9e9e189fb
Ellisvelandia <eyis619l@gmail.com>
2023-01-10 18:26:33 -0500

    datasocials

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,9 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
+import {socials} from "../data/socialData";
 import { motion } from "framer-motion";
 
 const Footer = () => {
-  const socials = [
-    {
-      id: 1,
-      url: "https://www.linkedin.com/in/ellisvelandia/",
-    },
-    {
-      id: 2,
-      url: "https://t.me/+573125668800",
-    },
-    {
-      id: 3,
-      url: "https://wa.me/+573125668800",
-    },
-    {
-      id: 4,
-      url: "https://github.com/Ellisvelandia",
-    },
-  ];
-
   return (
     <footer className="absolute bottom-0 w-full p-5 flex items-center justify-center my-6">
       <motion.div
---
+++ src/data/socialData.js
@@ -0,0 +1,18 @@
+ export const socials = [
+  {
+    id: 1,
+    url: "https://www.linkedin.com/in/ellisvelandia/",
+  },
+  {
+    id: 2,
+    url: "https://t.me/+573125668800",
+  },
+  {
+    id: 3,
+    url: "https://wa.me/+573125668800",
+  },
+  {
+    id: 4,
+    url: "https://github.com/Ellisvelandia",
+  },
+];
