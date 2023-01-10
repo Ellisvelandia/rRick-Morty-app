2e97ca6dff9e63dc42cc38f0eb3cd3fd2a004b5a
Ellisvelandia <eyis619l@gmail.com>
2023-01-09 21:16:40 -0500

    rick&morty

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,23 +4,25 @@ import video from "../assets/video.mp4";
 const Home = () => {
   return (
     <>
-      <header className="w-full flex align-center justify-center my-8">
-        <img
-          src="https://media.cdn.adultswim.com/uploads/20210428/21428161947-rick-and-morty-logo-png.png"
-          alt="title"
-        />
-      </header>
-      <Link to="/characters">
-        <h3 className="text-4xl font-bold text-white">view all Characters</h3>
-      </Link>
-      <section className="relative h-96">
+      <header className="w-full flex align-center justify-center my-8 h-96 mask2">
         <video
           src={video}
           autoPlay
           loop
-          className="w-full h-full center object-cover p-4 "
+          muted
+          className="w-full 2xl:h-[420px] h-full p-4 object-cover mask1"
         ></video>
-      </section>
+        <div className="absolute w-full flex align-center justify-center flex-col">
+          <img
+            src="https://media.cdn.adultswim.com/uploads/20210428/21428161947-rick-and-morty-logo-png.png"
+            alt="title"
+            className="w-96 flex m-auto  h-50 object-cover"
+          />
+        </div>
+      </header>
+      <Link to="/characters">
+        <h3 className="text-5xl font-bold text-white">view all Characters</h3>
+      </Link>
     </>
   );
 };
